787ad6a78a3531c46e0291baaf264982178e57ab
francelaquino <34216937+francelaquino@users.noreply.github.com>
2018-07-23 16:03:56 +0300

    403

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -12,24 +12,7 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const cors = require("cors");
 const express = require('express');
-//const maps = require('@google/maps')
-/*
-var googleMapsClient = require('@google/maps').createClient({
-    key: 'xxx',
-    Promise: Promise
-  });
-
-  console.log(snapadd + " snap user address (((((((())))))))");
-  if(snapadd != null || typeof snapadd != undefined) {
-          googleMapsClient.geocode({
-            address: snapadd
-          }).asPromise()
-              .then(response =>  console.log(response.json.results));
-              .catch(err => console.log(err))
-  }
-//}))
-//console.log(typeof user + 'type of');
-*/
+var NodeGeocoder = require('node-geocoder');
 admin.initializeApp(functions.config().firebase);
 const router = express.Router();
 const db = admin.database();
@@ -48,13 +31,32 @@ const getDistance = (lat1, long1, lat2, long2) => {
     let d = R * c;
     return d;
 };
+const getGeoLocation = (latitude, longtitude) => __awaiter(this, void 0, void 0, function* () {
+    let address = "";
+    let options = {
+        provider: 'google',
+        httpAdapter: 'https',
+        apiKey: 'AIzaSyCHZ-obEHL8TTP4_8vPfQKAyzvRrrlmi5Q',
+        formatter: null
+    };
+    let geocoder = NodeGeocoder(options);
+    yield geocoder.reverse({ lat: latitude, lon: longtitude })
+        .then(function (response) {
+        let add = JSON.parse(response);
+        address = response[0].formattedAddress;
+    })
+        .catch(function (err) {
+        address = "";
+    });
+    return address;
+});
 const processPlaceAlert = (lat1, lon1, address, userid, firstname) => __awaiter(this, void 0, void 0, function* () {
     let placeArriveNotify = [];
     let placeLeftNotify = [];
     yield db.ref("placealert/" + userid).once("value").then(function (snapshot) {
         return __awaiter(this, void 0, void 0, function* () {
             yield snapshot.forEach((childSnapshot) => {
-                if (childSnapshot.val().arrives === true) {
+                if (childSnapshot.val().arrives == true) {
                     let distance = getDistance(Number(lat1), Number(lon1), Number(childSnapshot.val().latitude), Number(childSnapshot.val().longitude));
                     if (Number(distance) <= 10) {
                         placeArriveNotify.push({
@@ -66,7 +68,7 @@ const processPlaceAlert = (lat1, lon1, address, userid, firstname) => __awaiter(
                         });
                     }
                 }
-                if (childSnapshot.val().leaves === true) {
+                if (childSnapshot.val().leaves == true) {
                     placeLeftNotify.push({
                         placeowner: childSnapshot.val().placeowner,
                         placeid: childSnapshot.val().placeid,
@@ -82,7 +84,7 @@ const processPlaceAlert = (lat1, lon1, address, userid, firstname) => __awaiter(
         yield placeArriveNotify.forEach((place) => __awaiter(this, void 0, void 0, function* () {
             yield db.ref('placeshistory/' + place.placeowner + '/' + place.userid + '/' + place.placeid).once("value", function (dataSnapshot) {
                 return __awaiter(this, void 0, void 0, function* () {
-                    if (dataSnapshot.val() === null) {
+                    if (dataSnapshot.val() == null) {
                         yield db.ref('placeshistory/' + place.placeowner + '/' + place.userid + '/' + place.placeid).set({
                             address: address,
                             latitude: place.latitude,
@@ -139,12 +141,12 @@ const saveLocation = (lat1, lon1, address, userid, dateadded) => __awaiter(this,
     let lat2 = "";
     let lon2 = "";
     yield db.ref("users/" + userid).once("value").then(function (snapshot) {
-        if (snapshot.val() !== null && snapshot.val().latitude != undefined && snapshot.val().longitude != undefined) {
+        if (snapshot.val() !== null && snapshot.val().latitude !== undefined && snapshot.val().longitude !== undefined) {
             lat2 = snapshot.val().latitude;
             lon2 = snapshot.val().longitude;
         }
     });
-    if (lat2 == "" || lon2 == "" || lat2 === undefined || lon2 == undefined) {
+    if (lat2 == "" || lon2 == "" || lat2 == undefined || lon2 == undefined) {
         yield db.ref('locations/' + userid).push({
             lat: lat1,
             lon: lon1,
@@ -180,130 +182,32 @@ app.get('/appendLocation', function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         let lat1 = req.query.lat;
         let lon1 = req.query.lon;
-        let address = req.query.address;
         let userid = req.query.userid;
         let dateadded = req.query.dateadded;
         let firstname = req.query.firstname;
+        let address = yield getGeoLocation(Number(lat1), Number(lon1));
         yield processPlaceAlert(lat1, lon1, address, userid, firstname);
         yield saveLocation(lat1, lon1, address, userid, dateadded);
-        // let geocoder = new maps.Geocoder;
         let latlng = { lat: parseFloat(lat1), lng: parseFloat(lon1) };
-        /*geocoder.geocode({'location': latlng}, function(results, status) {
-            res.send(results);
-          });*/
         res.send("updated");
     });
 });
-const api = functions.https.onRequest(app);
-module.exports = {
-    api
-};
-/*
-const rad=(x)=> {
-    return x * Math.PI / 180;
-};
-const getDistance=(lat1,long1,lat2,long2) =>{
-    let R = 6378137;
-    let dLat = rad(lat2 - lat1);
-    let dLong = rad(long2 - long1);
-    let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(rad(lat1)) * Math.cos(rad(lat2)) *
-      Math.sin(dLong / 2) * Math.sin(dLong / 2);
-      let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      let d = R * c;
-    return d;
-  };
-
-  */
-/*
-export const saveLocation1 = functions.https.onRequest((request, response) => {
-
-})
-export const saveLocation = functions.https.onRequest((request, response) => {
-
-  let lat2="";
-  let lon2="";
-
-  let lat1=request.query.lat;
-  let lon1=request.query.lon;
-  let address = request.query.address;
-  let userid=request.query.userid;
-  let dateadded=request.query.dateadded;
- 
-
-
-  
-  let parentPromise=new Promise((resolve,reject)=>{
-      db.ref("users/"+userid).once("value").then(function(snapshot) {
-          if(snapshot.val()!==null && snapshot.val().latitude!=undefined && snapshot.val().longitude!=undefined){
-              lat2=snapshot.val().latitude;
-              lon2=snapshot.val().longitude;
-              resolve();
-          }else{
-              resolve();
-          }
-          
-      });
-  }).then(()=>{
-      if(lat2=="" || lon2=="" || lat2===undefined || lon2==undefined){
-          db.ref('locations/'+userid).push({
-              lat : lat1,
-              lon : lon1,
-              address : address,
-              dateadded : dateadded});
-          
-          db.ref("users").child(userid).update({
-                  address:address,
-                  latitude:lat1,
-                  longitude:lon1,
-                  lastmovement : dateadded
-              })
-
-      }else{
-          let childPromise = new Promise((resolve,reject)=>{
-              let movement=getDistance(Number(lat1),Number(lon1),Number(lat2),Number(lon2));
-              
-              resolve(movement);
-          }).then(function(movement){
-              if(Number(movement)>=10){
-                  
-                  db.ref('locations/'+userid).push({
-                      lat : lat1,
-                      lon : lon1,
-                      address : address,
-                      dateadded : dateadded});
-                  
-                  db.ref("users").child(userid).update({
-                          address:address,
-                          latitude:lat1,
-                          longitude:lon1,
-                          lastmovement : dateadded
-                      })
-              }
-          })
-      }
-      response.send("Location saved");
-      
-  });
-  
-  
-});
-*/
-app.get('/getLastLocation', function (req, res) {
+app.get('/sendNotification', function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
-        var registrationToken = "dEIg-95Y_xY:APA91bEhFFNwhQWmfPpX0yOj2HG8UuvErxvrJpxlbXqmf-hGm_2b7oMHDMYrRUyKQo46_inBv-C1-z7RB63WADwD2m54Z-wgeBf9ay53GOyGwfb2n7u_ycecV6BEXqSkvxe_Q5gPuHdURd_2e_YqF31gs01X1vIDBA";
-        var payload = {
+        let token = req.query.token;
+        let message = {
             notification: {
                 title: "Places Movement",
                 body: "This is the body of the notification message."
             }
         };
-        var options = {
+        let options = {
             priority: "high",
             sound: "default",
             timeToLive: 60 * 60 * 24
         };
-        admin.messaging().sendToDevice(registrationToken, payload, options)
+        //admin.messaging().send(message)
+        admin.messaging().sendToDevice(token, message, options)
             .then(function (response) {
             console.log("Successfully sent message:", response);
         })
@@ -313,4 +217,49 @@ app.get('/getLastLocation', function (req, res) {
         res.send("Location saved");
     });
 });
+app.get('/getLastLocation', function (req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        var options = {
+            provider: 'google',
+            // Optional depending on the providers
+            httpAdapter: 'https',
+            apiKey: 'AIzaSyCHZ-obEHL8TTP4_8vPfQKAyzvRrrlmi5Q',
+            formatter: null // 'gpx', 'string', ...
+        };
+        var geocoder = NodeGeocoder(options);
+        geocoder.reverse({ lat: 45.767, lon: 4.833 })
+            .then(function (response) {
+            console.log(response);
+            res.send(response);
+        })
+            .catch(function (err) {
+            console.log(err);
+        });
+        res.send("Done");
+    });
+});
+app.get('/getLastLocation1', function (req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        var options = {
+            provider: 'google',
+            httpAdapter: 'https',
+            apiKey: 'AIzaSyCHZ-obEHL8TTP4_8vPfQKAyzvRrrlmi5Q',
+            formatter: null // 'gpx', 'string', ...
+        };
+        var geocoder = NodeGeocoder(options);
+        geocoder.reverse({ lat: 45.767, lon: 4.833 })
+            .then(function (response) {
+            console.log(response);
+            console.log(response[0].formattedAddress);
+        })
+            .catch(function (err) {
+            console.log(err);
+        });
+        res.send("Done");
+    });
+});
+const api = functions.https.onRequest(app);
+module.exports = {
+    api
+};
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,24 +2,7 @@ import * as functions from 'firebase-functions';
 const admin = require('firebase-admin');
 const cors = require("cors")
 const express = require('express');
-//const maps = require('@google/maps')
-/*
-var googleMapsClient = require('@google/maps').createClient({
-    key: 'xxx',
-    Promise: Promise
-  });
-
-  console.log(snapadd + " snap user address (((((((())))))))");
-  if(snapadd != null || typeof snapadd != undefined) {
-          googleMapsClient.geocode({
-            address: snapadd
-          }).asPromise()
-              .then(response =>  console.log(response.json.results));
-              .catch(err => console.log(err))
-  }   
-//}))
-//console.log(typeof user + 'type of');
-*/
+var NodeGeocoder = require('node-geocoder');
 
 admin.initializeApp(functions.config().firebase);
 
@@ -41,12 +24,33 @@ const getDistance=(lat1,long1,lat2,long2) => {
       let d = R * c;
     return d; 
   };
+
+  const getGeoLocation=async (latitude,longtitude) => {
+    let address="";
+    let options = {
+        provider: 'google',
+        httpAdapter: 'https', 
+        apiKey: 'AIzaSyCHZ-obEHL8TTP4_8vPfQKAyzvRrrlmi5Q', 
+        formatter: null
+      };
+      let geocoder = NodeGeocoder(options);
+      await geocoder.reverse({lat:latitude, lon:longtitude})
+        .then(function(response) {
+            let add=JSON.parse(response);
+            address = response[0].formattedAddress;
+        })
+        .catch(function(err) {
+            address="";
+        });
+
+        return address;
+  };
   const processPlaceAlert=async (lat1,lon1,address,userid,firstname)=>{
     let placeArriveNotify:any=[];
     let placeLeftNotify:any=[];
     await db.ref("placealert/"+userid).once("value").then(async function(snapshot) {
         await snapshot.forEach((childSnapshot) =>{
-            if(childSnapshot.val().arrives===true){
+            if(childSnapshot.val().arrives==true){
                 let distance= getDistance(Number(lat1),Number(lon1),Number(childSnapshot.val().latitude),Number(childSnapshot.val().longitude));
                 if(Number(distance)<=10){
                     placeArriveNotify.push({
@@ -59,7 +63,7 @@ const getDistance=(lat1,long1,lat2,long2) => {
                    
                 }
             }
-            if(childSnapshot.val().leaves===true){
+            if(childSnapshot.val().leaves==true){
                 placeLeftNotify.push({
                     placeowner : childSnapshot.val().placeowner,
                     placeid : childSnapshot.val().placeid,
@@ -74,7 +78,7 @@ const getDistance=(lat1,long1,lat2,long2) => {
         if(placeArriveNotify.length>0){
             await placeArriveNotify.forEach(async place => {
                 await db.ref('placeshistory/'+place.placeowner+'/'+place.userid+'/'+place.placeid).once("value", async function(dataSnapshot) {
-                    if(dataSnapshot.val()===null){
+                    if(dataSnapshot.val()==null){
                         await db.ref('placeshistory/'+place.placeowner+'/'+place.userid+'/'+place.placeid).set({ 
                             address : address,
                             latitude : place.latitude,
@@ -138,14 +142,14 @@ const saveLocation=async (lat1,lon1,address,userid,dateadded)=>{
    
     
     await db.ref("users/"+userid).once("value").then(function(snapshot) {
-            if(snapshot.val()!==null && snapshot.val().latitude!=undefined && snapshot.val().longitude!=undefined){
+            if(snapshot.val()!==null && snapshot.val().latitude!==undefined && snapshot.val().longitude!==undefined){
                 lat2=snapshot.val().latitude;
                 lon2=snapshot.val().longitude;
             }
             
         });
     
-        if(lat2=="" || lon2=="" || lat2===undefined || lon2==undefined){
+        if(lat2=="" || lon2=="" || lat2==undefined || lon2==undefined){
             await db.ref('locations/'+userid).push({ 
                 lat : lat1,
                 lon : lon1,
@@ -182,20 +186,16 @@ const saveLocation=async (lat1,lon1,address,userid,dateadded)=>{
 app.get('/appendLocation', async function (req, res) {
     let lat1=req.query.lat;
     let lon1=req.query.lon;
-    let address = req.query.address;
     let userid=req.query.userid;
     let dateadded=req.query.dateadded;
     let firstname=req.query.firstname;
+
+    let address = await getGeoLocation(Number(lat1),Number(lon1));
     
     await processPlaceAlert(lat1,lon1,address,userid,firstname);
     await saveLocation(lat1,lon1,address,userid,dateadded);
-   // let geocoder = new maps.Geocoder;
     let latlng = {lat:  parseFloat(lat1), lng: parseFloat(lon1)};
-
-    /*geocoder.geocode({'location': latlng}, function(results, status) {
-        res.send(results);
-      });*/
-        res.send("updated");
+    res.send("updated");
 
     
  })
@@ -204,122 +204,26 @@ app.get('/appendLocation', async function (req, res) {
 
 
 
-const api = functions.https.onRequest(app)
-
-module.exports = {
-  api
-}
 
 
-/*
-const rad=(x)=> {
-    return x * Math.PI / 180;
-};
-const getDistance=(lat1,long1,lat2,long2) =>{
-    let R = 6378137;
-    let dLat = rad(lat2 - lat1);
-    let dLong = rad(long2 - long1);
-    let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(rad(lat1)) * Math.cos(rad(lat2)) *
-      Math.sin(dLong / 2) * Math.sin(dLong / 2);
-      let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      let d = R * c;
-    return d; 
-  };
+app.get('/sendNotification', async function (req, res) {
+    let token=req.query.token;
 
-  */
-
-  /*
-  export const saveLocation1 = functions.https.onRequest((request, response) => {
-
-  })
-export const saveLocation = functions.https.onRequest((request, response) => {
-
-    let lat2="";
-    let lon2="";
-
-    let lat1=request.query.lat;
-    let lon1=request.query.lon;
-    let address = request.query.address;
-    let userid=request.query.userid;
-    let dateadded=request.query.dateadded;
-   
-
-
-    
-    let parentPromise=new Promise((resolve,reject)=>{
-        db.ref("users/"+userid).once("value").then(function(snapshot) {
-            if(snapshot.val()!==null && snapshot.val().latitude!=undefined && snapshot.val().longitude!=undefined){
-                lat2=snapshot.val().latitude;
-                lon2=snapshot.val().longitude;
-                resolve();
-            }else{
-                resolve();
-            }
-            
-        });
-    }).then(()=>{
-        if(lat2=="" || lon2=="" || lat2===undefined || lon2==undefined){
-            db.ref('locations/'+userid).push({ 
-                lat : lat1,
-                lon : lon1,
-                address : address,
-                dateadded : dateadded});
-            
-            db.ref("users").child(userid).update({
-                    address:address,
-                    latitude:lat1,
-                    longitude:lon1,
-                    lastmovement : dateadded
-                })
-
-        }else{
-            let childPromise = new Promise((resolve,reject)=>{
-                let movement=getDistance(Number(lat1),Number(lon1),Number(lat2),Number(lon2));
-                
-                resolve(movement);
-            }).then(function(movement){
-                if(Number(movement)>=10){
-                    
-                    db.ref('locations/'+userid).push({ 
-                        lat : lat1,
-                        lon : lon1,
-                        address : address,
-                        dateadded : dateadded});
-                    
-                    db.ref("users").child(userid).update({
-                            address:address,
-                            latitude:lat1,
-                            longitude:lon1,
-                            lastmovement : dateadded
-                        })
-                }
-            })
-        }
-        response.send("Location saved");
-        
-    });
-    
-    
-});
-*/
-app.get('/getLastLocation', async function (req, res) {
-    var registrationToken="dEIg-95Y_xY:APA91bEhFFNwhQWmfPpX0yOj2HG8UuvErxvrJpxlbXqmf-hGm_2b7oMHDMYrRUyKQo46_inBv-C1-z7RB63WADwD2m54Z-wgeBf9ay53GOyGwfb2n7u_ycecV6BEXqSkvxe_Q5gPuHdURd_2e_YqF31gs01X1vIDBA";
-
-    var payload = {
+    let message = {
         notification: {
           title: "Places Movement",
           body: "This is the body of the notification message."
         }
       };
       
-       var options = {
-        priority: "high",
-        sound: "default",
-        timeToLive: 60 * 60 *24
+       let options = {
+            priority: "high",
+            sound: "default",
+            timeToLive: 60 * 60 *24
       };
       
-      admin.messaging().sendToDevice(registrationToken, payload, options)
+      //admin.messaging().send(message)
+      admin.messaging().sendToDevice(token, message, options)
       .then(function(response) {
         console.log("Successfully sent message:", response);
       })
@@ -329,3 +233,58 @@ app.get('/getLastLocation', async function (req, res) {
      
         res.send("Location saved");
 });
+
+
+app.get('/getLastLocation', async function (req, res) {
+    var options = {
+        provider: 'google',
+      
+        // Optional depending on the providers
+        httpAdapter: 'https', // Default
+        apiKey: 'AIzaSyCHZ-obEHL8TTP4_8vPfQKAyzvRrrlmi5Q', // for Mapquest, OpenCage, Google Premier
+        formatter: null         // 'gpx', 'string', ...
+      };
+      var geocoder = NodeGeocoder(options);
+      geocoder.reverse({lat:45.767, lon:4.833})
+  .then(function(response) {
+    console.log(response);
+    res.send(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+   
+      res.send("Done");
+        
+
+});
+
+app.get('/getLastLocation1', async function (req, res) {
+    var options = {
+        provider: 'google',
+        httpAdapter: 'https', // Default
+        apiKey: 'AIzaSyCHZ-obEHL8TTP4_8vPfQKAyzvRrrlmi5Q', // for Mapquest, OpenCage, Google Premier
+        formatter: null         // 'gpx', 'string', ...
+      };
+      var geocoder = NodeGeocoder(options);
+      geocoder.reverse({lat:45.767, lon:4.833})
+  .then(function(response) {
+    console.log(response);
+    console.log(response[0].formattedAddress)
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+   
+      res.send("Done");
+        
+
+});
+
+const api = functions.https.onRequest(app)
+
+module.exports = {
+  api
+}
